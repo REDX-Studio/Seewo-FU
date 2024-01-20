34ec06c5156b879bb5c5de9826928042078440ae
MichaelJaycox <129545318+MikeChen-CJ@users.noreply.github.com>
2024-01-20 13:02:38 +0800

    Add files via upload

---
+++ SeewoFU_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef SEEWOFU_PRIVATE_H
+#define SEEWOFU_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.1.3.0"
+#define VER_MAJOR	0
+#define VER_MINOR	1
+#define VER_RELEASE	3
+#define VER_BUILD	0
+#define COMPANY_NAME	"DOTS_stdio@2023"
+#define FILE_VERSION	"0.1.3.0"
+#define FILE_DESCRIPTION	""
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"SeewoFU.exe"
+#define PRODUCT_NAME	"SeewoFU"
+#define PRODUCT_VERSION	"0.1.3.0"
+
+#endif /*SEEWOFU_PRIVATE_H*/
